59d98c3d12a38e4770d60f689fadd1a342692a7d
louisa5ophie <louisa.strebe@gmx.de>
2026-09-24 14:58:00 +0200

    Update route to handle wildcard paths

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.post("/api/complete",auth,async(req,res)=>{
 });
 
 app.use(express.static(path.join(__dirname,"public")));
-app.get("*",(req,res)=>res.sendFile(path.join(__dirname,"public","index.html")));
+app.get("/{*splat}",(req,res)=>res.sendFile(path.join(__dirname,"public","index.html")));
 
 const port=process.env.PORT||8080;
 init().then(()=>app.listen(port,"0.0.0.0",()=>console.log("HaushaltsTasse listening on",port)))
